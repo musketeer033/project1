8862cb40ac0cb7e250fc6732136a81dfeec2ef80
shivdeep2002 <iamshivdeep@gmail.com>
2024-07-17 18:49:27 +0530

    hello

--- src/components/Job/Navbar.jsx
+++ src/components/Job/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = ({ onLogout }) => {
   return (
     <div className="flex justify-between items-center">
       <div className="flex items-center">
-        <div className="bg-gray-200 rounded-full h-12 w-12 flex items-center justify-center text-gray-600 font-bold text-xl mr-3">
+        <div className="bg-gray-200 rounded-full h-16 w-16 flex items-center justify-center text-gray-600 font-bold text-xl mr-3">
           <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/Coat_of_arms_of_Chhattisgarh.svg/300px-Coat_of_arms_of_Chhattisgarh.svg.png' alt='' />
         </div>
         <div className="text-lg font-bold">Your Company Name</div>
